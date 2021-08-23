3b3564ff5f05d7e20fbe0d5284add0a075e00826
Exeon <actuallyexeon@protonmail.com>
2021-08-23 10:00:26 -0500

    Made Better README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cherrykernel
 Small, simple, and fast kernel written in Rust.
+
+Checklist:
+- [x] Make kernel base
+- [x] Basic framebuffer
+- [ ] APIC (Priority for now)
+- [ ] Disk Reading
--- src/interrupts/apic.rs
+++ src/interrupts/apic.rs
@@ -1,23 +1,3 @@
 // cherrykernel's apic module
-// apic thing or something, doesn't work right now
+// this was a major help > https://github.com/gamozolabs/chocolate_milk/blob/master/kernel/src/apic.rs
 
-/*
-use x2apic::lapic::{LocalApic, LocalApicBuilder, xapic_base};
-
-pub fn apic() {
-    let apic_physical_address: u64 = unsafe { xapic_base() };
-    let apic_virtual_address: u64 =
-
-    let lapic = LocalApicBuilder::new()
-        .timer_vector(timer_index)
-        .error_vector(error_index)
-        .spurious_vector(spurious_index)
-        .set_xapic_base(apic_virtual_address)
-        .build()
-        .unwrap_or_else(|err| panic!("{}", err));
-
-    unsafe {
-        lapic.enable();
-    }
-}
-*/
